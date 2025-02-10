1069c7a7cf5eb462c1335b29924b0dc9aa052960
Ed Marquez <72571340+ed-marquez@users.noreply.github.com>
2025-02-10 18:31:26 -0500

    Minor updates
    Signed-off-by: Ed Marquez <72571340+ed-marquez@users.noreply.github.com>

--- queries/getAccountActivity.js
+++ queries/getAccountActivity.js
@@ -19,23 +19,6 @@ query getAccountActivity($accountIds: [bigint!], $startTime: bigint!, $endTime: 
   }
 }`;
 
-// export const GET_ACCOUNT_ACTIVITY_QUERY = `
-// query getAccountActivity($accountIds: [bigint!], $startTime: bigint!, $endTime: bigint!) {
-//   transaction(
-//     where: {
-//       payer_account_id: {_in: $accountIds},
-//       consensus_timestamp: {_gte: $startTime, _lte: $endTime}
-//     }
-//     order_by: {consensus_timestamp: asc}
-//   ) {
-//     consensus_timestamp
-//     id
-//     type
-//     result
-//     payer_account_id
-//   }
-// }`;
-
 // // SAMPLE OUTPUT
 // {
 //   "data": {
